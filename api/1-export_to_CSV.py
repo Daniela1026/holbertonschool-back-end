4e3be77a6f5dc696f525872827b51930e681772f
Daniela Cartagena Restrepo <106104377+Daniela1026@users.noreply.github.com>
2022-12-14 12:36:52 -0500

    Create export csv

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -1,19 +1,37 @@
 #!/usr/bin/python3
-""" CSV EXPORT"""
+"""Python script to export data in the CSV format"""
+
 import csv
-from requests import get
 from sys import argv
+from requests import get
 
 
-def csv_expor():
-    """CSV EXPORT"""
+def export_cvs():
 
-    user_url = get("https://jsonplaceholder.typicode.com/users").json()
-    task_url = get("https://jsonplaceholder.typicode.com/todos").json()
+    url_base = "https://jsonplaceholder.typicode.com/"
 
-    with open('{}.csv'.format(argv[1]), 'w', newline='') as f:
-        writ = csv.writer(f, quoting=csv.QUOTE_ALL)
-        
-        for task in task_url:
-            writ.writerow([user_url['id'], user_url['username'],
-                                task_url['completed'], task_url['title']])
+    url_user = "{}users/{}".format(url_base, argv[1])
+    res = get(url_user)
+    json_user = res.json()
+    username = json_user.get("username")
+
+    todos = "{}todos?userId={}".format(url_base, argv[1])
+    res = get(todos)
+    json_task = res.json()
+    done_task = []
+
+    for todo in json_task:
+        done_task.append(
+            [argv[1], username, todo.get("completed"), todo.get("title")])
+
+    csv_file = argv[1] + ".csv"
+    with open(csv_file, mode="w", encoding="utf-8") as f:
+        writer = csv.writer(f, delimiter=',', quotechar='"',
+                            quoting=csv.QUOTE_ALL)
+        for task in done_task:
+            writer.writerow(task)
+
+
+if __name__ == "__main__":
+    export_cvs()
+
