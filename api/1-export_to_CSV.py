cbace5e061e76df6c864d43bf27b948388de31ae
davidperlaza14 <davidperlaza14@gmail.com>
2022-12-16 10:24:49 -0500

    1. Export to CSV

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -3,36 +3,51 @@
 
 
 import csv
+import json
+import requests
 from sys import argv
-from requests import get
-
-
-def csv_export():
-    """csv export"""
-
-    url_base = "https://jsonplaceholder.typicode.com/"
-
-    user = "{}users/{}".format(url_base, argv[1])
-    res = get(user)
-    json_user = res.json()
-    username = json_user.get("username")
-
-    todos = "{}todos?userId={}".format(url_base, argv[1])
-    res = get(todos)
-    json_task = res.json()
-    done_task = []
-
-    for todo in json_task:
-        done_task.append(
-            [argv[1], username, todo.get("completed"), todo.get("title")])
-
-    csv_file = argv[1] + ".csv"
-    with open(csv_file, mode="w", encoding="utf-8") as f:
-        writer = csv.writer(f, delimiter=',', quotechar='"',
-                            quoting=csv.QUOTE_ALL)
-        for task in done_task:
-            writer.writerow(task)
 
 
 if __name__ == "__main__":
-    csv_export()
+
+    res = requests.get(
+        "https://jsonplaceholder.typicode.com/todos/?userId={}".format(argv[1])
+    )
+    tsks = json.loads(res.text)
+    usrs = requests.get("https://jsonplaceholder.typicode.com/users")
+    us = json.loads(usrs.text)
+    res_us = {}
+    counter = 0
+    csv_row = []
+    nid = int(argv[1])
+
+    for line in us:
+        counter += 1
+        res_us.update(line)
+        if counter == nid:
+            break
+    name_us = res_us["username"]
+
+    my_tsks = [
+        (i["completed"], i["title"]) for i in tsks
+        if "completed" or "title" in i
+    ]
+
+    with open('{}.csv'.format(nid), 'w') as fp:
+        writer = csv.writer(fp, quoting=csv.QUOTE_ALL)
+        writer.writerow(csv_row)
+        for i, j in my_tsks:
+            if i is True:
+                csv_row.append("{}".format(nid))
+                csv_row.append("{}".format(name_us))
+                csv_row.append("True")
+                csv_row.append("{}".format(j))
+                writer.writerow(csv_row)
+                csv_row = []
+            else:
+                csv_row.append("{}".format(nid))
+                csv_row.append("{}".format(name_us))
+                csv_row.append("False")
+                csv_row.append("{}".format(j))
+                writer.writerow(csv_row)
+                csv_row = []
